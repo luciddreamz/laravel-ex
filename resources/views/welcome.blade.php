87e53d7b81f0a2576c3ba5b12850491eade061e1
yr <ye.123789456@gmail.com>
2019-01-30 00:14:53 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 888.5</div>
+                <div class="title">Laravel 88800.5</div>
             </div>
         </div>
     </body>
